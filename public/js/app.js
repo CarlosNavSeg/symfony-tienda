edbe36f9024f937534984704d4d6b7c7f645fd8e
CarlosNavSeg <carlos.raco@hotmail.com>
2023-01-10 10:36:27 +0100

    Arreglado el modal de libro.

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,13 @@
     const infoBook = $("#infobook");
     $("a.open-info-book").click(function(event) {
       event.preventDefault();
-      let id = $( this ).prop('id');
+      let id = $( this ).attr('id');
       const href = `/api/show/${id}`;
       $.get( href, function(data) {
         $( infoBook ).find( "#bookTitle" ).text(data.title);
         $( infoBook ).find( "#bookPrice" ).text(data.price);
         $( infoBook ).find( "#bookAuthor" ).text(data.author);
-        $( infoBook).find( "#bookPhoto").text(data.photo)
+        $( infoBook).find( "#bookPhoto").attr("src", "/img/" + data.photo)
         $( infoBook).find( "#bookPrice").text(data.price)
         $( infoBook ).find( "#bookpublishingHouse" ).text(data.publishing_house);
         infoBook.modal('show');
